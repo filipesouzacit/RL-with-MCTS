cca1a8a79e5973a30b423c45a090e2473975c189
filipesouzacit <56239650+filipesouzacit@users.noreply.github.com>
2021-01-19 10:59:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -41,11 +41,11 @@ args = {
      1.0: 'BLACK', 
     -1.0: 'WHITE',
     'batchSize': 64,
-    'numIters': 500,                                # Total number of training iterations
+    'numIters': 20,                                # Total number of training iterations
     'num_simulations': 100,                         # Total number of MCTS simulations to run when deciding on a move to play
-    'numEps': 100,                                  # Number of full games (episodes) to run during each iteration
+    'numEps': 81,                                  # Number of full games (episodes) to run during each iteration
     'numItersForTrainExamplesHistory': 20,
-    'epochs': 50,                                    # Number of epochs of training per iteration
+    'epochs': 2,                                    # Number of epochs of training per iteration
     'checkpointPath': 'model.hdf5'                  # location to save latest set of weights
 }
 
